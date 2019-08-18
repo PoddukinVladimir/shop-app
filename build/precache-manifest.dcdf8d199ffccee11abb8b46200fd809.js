1cdff5e5ac5764047c7d189dbd5b5d960a9f0059
Vladimir <poddukinvladimir@gmail.com>
2019-08-18 23:45:45 +0300

    add build files for gh-pages presentation

---
+++ build/precache-manifest.dcdf8d199ffccee11abb8b46200fd809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3611e3efd4a485d171a6fdc47d6860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c357f4b84e04fc576c32",
+    "url": "/static/css/main.b03a87ce.chunk.css"
+  },
+  {
+    "revision": "2c74ee43dbf6dd69a2ce",
+    "url": "/static/js/2.a3e4b514.chunk.js"
+  },
+  {
+    "revision": "c357f4b84e04fc576c32",
+    "url": "/static/js/main.df8a4ee0.chunk.js"
+  },
+  {
+    "revision": "4a90d6a26b2dba82f629",
+    "url": "/static/js/runtime~main.61b2e78c.js"
+  }
+]);
